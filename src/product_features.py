0462ffecbfcaa43e5c8ceafd4944cc00649128fe
dyee <darian.yee@hotmail.com>
2026-03-19 20:42:31 -0400

    Completed aisle and department features

--- src/product_features.py
+++ src/product_features.py
@@ -1,11 +1,10 @@
 import pandas as pd 
 
-#3.2.1 Product Total Purchases
-#3.2.2 Product Reorder Ratio
-
 def get_product_features(prior_orders):
     
     product_features = prior_orders.groupby(['product_id']).agg(
+        #3.2.1 Product Total Purchases
+        #3.2.2 Product Reorder Ratio
         p_total_purchases = ('product_id', 'count'),
         p_reorder_ratio = ('reordered', 'mean')
     ).reset_index()
