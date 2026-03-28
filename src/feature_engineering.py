86b3a8c78413b8413f7ac3e8c32d9689f27e95d2
dyee <darian.yee@hotmail.com>
2026-03-27 20:41:23 -0400

    cleaned up markdown notes

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -110,28 +110,27 @@ def get_aisle_department_features(prior_orders):
     
     #3.4.1 Aisle reorder ratio
     #3.4.2 Aisle total orders
-    #3.4.3 Department reorder ratio
-    #3.4.4 Department total orders
-    #3.4.5 User-Aisle reorder ratio
-    #3.4.6 User-Aisle total orders
-    #3.4.7 User-Department reorder ratio
-    #3.4.8 User-Department total orders
-
     aisle_features = prior_orders.groupby(['aisle_id']).agg(
         a_reorder_ratio = ('reordered', 'mean'),
         a_total_orders = ('product_id', 'count')
     ).reset_index()
 
+    #3.4.5 User-Aisle reorder ratio
+    #3.4.6 User-Aisle total orders
     user_aisle_features = prior_orders.groupby(['user_id', 'aisle_id']).agg(
         ua_reorder_ratio = ('reordered', 'mean'),
         ua_total_orders = ('product_id', 'count')
     ).reset_index()
 
+    #3.4.3 Department reorder ratio
+    #3.4.4 Department total orders
     department_features = prior_orders.groupby(['department_id']).agg(
         d_reorder_ratio = ('reordered', 'mean'),
         d_total_orders = ('product_id', 'count')
     ).reset_index()
 
+    #3.4.7 User-Department reorder ratio
+    #3.4.8 User-Department total orders
     user_department_features = prior_orders.groupby(['user_id', 'department_id']).agg(
         ud_reorder_ratio = ('reordered', 'mean'),
         ud_total_orders = ('product_id', 'count')
